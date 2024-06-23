1fae4dd5671b1f774e48e2e729aa9374a3d28cd5
Dharsanarengasamy <dharsana.rengasamy.mdu@gmail.com>
2024-06-23 15:56:10 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float versionType = 91.0f;
+		String developerType = "Google";
+		boolean isBetaValue = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("The Version type is "+versionType);
+		System.out.println("The developer type is "+developerType);
+        System.out.println("The Beta Value is "+isBetaValue);
+        System.out.println("The release year is "+releaseYear);
+        System.out.println("The shortcut key for type is "+shortcutKey);
+	}
+
+}
---
+++ day1/FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "FireFox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("The browser Version is "+browserVersion);
+		System.out.println("The browser Name is "+browserName);
+		System.out.println("Is browser is visible "+isVisible);	
+		System.out.println("The release year was "+releaseYear);
+		System.out.println("The browser Logo is "+browserLogo);
+		
+	}
+
+}
